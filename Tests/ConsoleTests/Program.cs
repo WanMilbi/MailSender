a39cc40968c773e79aee15fa68afc06dcfa1cbb8
Милованов Вячеслав <lostvan.fan@gmail.com>
2022-09-26 15:56:12 +0300

    Тестовый код отправки почты в консольном приложении

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net.Mail;
+using System.Net;
+using System.Net.Http.Headers;
+using Microsoft.VisualBasic;
 
 namespace ConsoleTests
 {
@@ -6,7 +10,28 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            
+            var from = new MailAddress("WanMilBi@yandex.ru", "WanMilBi");
+            
+            var to = new MailAddress("lostvan.fan@gmail.com", "WanMilBi");
+
+            var messege = new MailMessage(from, to);
+            //var msg = new MailAddress("user@server.ru", "qwe@ASD.ru");
+
+            messege.Subject = "Заголовок письма от " + DateTime.Now;
+            messege.Body = "Текст тестового письма" + DateTime.Now;
+
+            var client = new SmtpClient("smtp.yandex.ru", 587);
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "user_name",
+                Password = "Password"
+            };
+
+            client.Send(messege);
+
         }
     }
 }
