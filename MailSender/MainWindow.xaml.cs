f843aebd421d6b38183d3ab0f56514d4e735ff26
Милованов Вячеслав <lostvan.fan@gmail.com>
2022-09-28 20:46:33 +0300

    Тестовое WPF-приложение для проверки возможности отправки почты

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -18,11 +18,8 @@ namespace MailSender
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow 
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
     }
 }
