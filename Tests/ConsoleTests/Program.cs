8fdb014e4a1a70c02178677393ef16e5325d1a90
Милованов Вячеслав <lostvan.fan@gmail.com>
2022-10-27 19:46:47 +0300

    Сервис-шифровальщик

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Net.Mail;
-using System.Net;
-using System.Net.Http.Headers;
-using Microsoft.VisualBasic;
+﻿using MailSender.lib.Interfaces;
+using MailSender.lib.Service;
 
 namespace ConsoleTests
 {
@@ -10,28 +7,13 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
-            
-            
-            var from = new MailAddress("WanMilBi@yandex.ru", "WanMilBi");
-            
-            var to = new MailAddress("lostvan.fan@gmail.com", "WanMilBi");
+            IEncryptorService cryptor = new Rfc2898Encryptor();
 
-            var messege = new MailMessage(from, to);
-            //var msg = new MailAddress("user@server.ru", "qwe@ASD.ru");
+            var str = "Hello World!";
+            const string password = "MailSender!";
 
-            messege.Subject = "Заголовок письма от " + DateTime.Now;
-            messege.Body = "Текст тестового письма" + DateTime.Now;
-
-            var client = new SmtpClient("smtp.yandex.ru", 587);
-
-            client.Credentials = new NetworkCredential
-            {
-                UserName = "user_name",
-                Password = "Password"
-            };
-
-            client.Send(messege);
-
+            var crypted_str = cryptor.Excrypt(str, password);
+            var decrypted_str = cryptor.Decrypt(crypted_str,password);
         }
     }
 }
