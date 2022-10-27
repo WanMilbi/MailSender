1df883069b3cc6acc1dd838605bf52809be9b868
Милованов Вячеслав <lostvan.fan@gmail.com>
2022-10-27 19:35:46 +0300

    Сервис-шифровальщик

---
+++ MailSender.lib/Interfaces/IEncryptorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Interfaces
+{
+    public interface IEncryptorService
+    {
+        string Excrypt (string str, string Password);
+        string Decrypt(string str, string Password);
+
+        byte[] Encrypt(byte[] data, string Password);
+        byte [] Decrypt(byte[] data, string Password);
+    }
+}
