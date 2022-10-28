23e099a1f0363cee08c165cd10293a85e8d5a316
Милованов Вячеслав <lostvan.fan@gmail.com>
2022-10-28 15:57:13 +0300

    Визуализация ошибок валидации

--- MailSender.lib/Models/Entity.cs
+++ MailSender.lib/Models/Entity.cs
@@ -5,12 +5,12 @@
         public int Id { get; set; }
     }
 
-    public abstract class NamedEntity
+    public abstract class NamedEntity : Entity
     {
-        public string Name { get; set; }
+        public virtual string Name { get; set; }
     }
 
-    public abstract class Person:NamedEntity
+    public abstract class Person : NamedEntity
     {
 public string Address { get; set; }
     }
---
+++ MailSender/Views/RecipientEditor.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+
+namespace MailSender.Views
+{
+    public partial class RecipientEditor 
+    {
+        public RecipientEditor() => InitializeComponent();
+
+        private void OnDataValidationError(object? Sender, ValidationErrorEventArgs E)
+        {
+            var control = (Control)Sender;
+            if (E.Action == ValidationErrorEventAction.Added)
+            {
+                control.ToolTip = E.Error.ErrorContent.ToString();
+            }
+            else
+            {
+                control.ClearValue(ToolTipProperty);
+            }
+        }
+    }
+}
