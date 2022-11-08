0716dcb522f59c96dd6e1400970203e8033afdd9
Милованов Вячеслав <lostvan.fan@gmail.com>
2022-11-08 16:27:53 +0300

    Модульные тесты

---
+++ Tests/MailSender.lib.Tests/Service/Rfc2898EncryptorTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Cryptography;
+using MailSender.lib.Interfaces;
+using MailSender.lib.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailSender.lib.Tests.Service
+{
+    [TestClass]
+    public class Rfc2898EncryptorTests
+    {
+        
+        private IEncryptorService _Encryptor = new Rfc2898Encryptor();
+        static Rfc2898EncryptorTests() {}
+        public Rfc2898EncryptorTests() {}
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext Context)
+        {
+
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext Context)
+        {
+
+        }
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+        //Выполняется сам модульный тест
+        [TestCleanup]
+        public void TestCleanup()
+        {
+
+        }
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+
+        }
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+
+        }
+        [TestMethod]
+        public void Encrypt_Hello_World_and_Decrypt_with_Password()
+        {
+            const string str = "Hello World";
+            const string password = "Password";
+            var encrypted_str = _Encryptor.Excrypt(str, password);
+            var decrypted_str = _Encryptor.Decrypt(encrypted_str, password);
+//var wrong_pass_decrypted = _Encryptor.Decrypt(encrypted_str, "QWE");
+
+            Assert.AreNotEqual(str, encrypted_str);
+            Assert.AreEqual(str, decrypted_str);
+            //Assert.AreNotEqual(str, wrong_pass_decrypted);
+
+        }
+        [TestMethod, ExpectedException(typeof(CryptographicException))]
+        public void Wrong_Decryption_throw_CryptographicExcrption()
+        {
+            const string str = "Hello World";
+            const string password = "Password";
+            var encrypted_str = _Encryptor.Excrypt(str, password);
+            var wrong_pass_decrypted = _Encryptor.Decrypt(encrypted_str, "QWE");
+        }
+    }
+}
---
+++ Tests/MailSender.lib.Tests/Service/TestEncoderTests.cs
@@ -0,0 +1,54 @@
+﻿using MailSender.lib.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailSender.lib.Tests.Service
+{
+    [TestClass]
+    public class TestEncoderTests
+    {
+        [TestMethod]
+        public void Encode_ABC_to_BCD_with_key_1()
+        {
+            // A-A-A = Arrange - Act - Assert
+
+
+            #region Arrange
+            const string str = "ABC";
+            const int key = 1;
+            const string execpted_str = "BCD";
+            #endregion
+
+            #region Act
+            var actual_str = TextEncoder.Encode(str, key);
+            #endregion
+
+            #region Assert
+Assert.AreEqual(execpted_str, actual_str);
+            
+
+            #endregion
+        }
+        [TestMethod]
+        public void Decode_BCD_to_ABC_with_key_1()
+        {
+            // A-A-A = Arrange - Act - Assert
+
+
+            #region Arrange
+            const string str = "BCD";
+            const int key = 1;
+            const string execpted_str = "ABC";
+            #endregion
+
+            #region Act
+            var actual_str = TextEncoder.Decode(str, key);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(execpted_str, actual_str);
+
+
+            #endregion
+        }
+    }
+}
