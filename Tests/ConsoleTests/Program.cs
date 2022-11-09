c95a2431ece3950894bdead73fd95f6825a443ee
Милованов Вячеслав <lostvan.fan@gmail.com>
2022-11-09 16:51:18 +0300

    Создание потока

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,5 +1,9 @@
-﻿using MailSender.lib.Interfaces;
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+using MailSender.lib.Interfaces;
 using MailSender.lib.Service;
+using Microsoft.VisualBasic;
 
 namespace ConsoleTests
 {
@@ -7,13 +11,38 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
-            IEncryptorService cryptor = new Rfc2898Encryptor();
+            var main_thread = Thread.CurrentThread;
+            var main_thread_id = main_thread.ManagedThreadId;
+            main_thread.Name = "Главный поток";
+            
+            var timer_thread = new Thread(TimeMethod);
+            timer_thread.Name = "Поток часов";
+            timer_thread.IsBackground = true;
+timer_thread.Start();
 
-            var str = "Hello World!";
-            const string password = "MailSender!";
-
-            var crypted_str = cryptor.Excrypt(str, password);
-            var decrypted_str = cryptor.Decrypt(crypted_str,password);
-        }
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Главный поток {i}");
+    Thread.Sleep(10);
+}
+            Console.WriteLine("Главный поток работу закончил");
+            Console.ReadLine();            
     }
+
+        private static void TimeMethod()
+        {
+            PrintThreadInfo();
+            while (true)
+            {
+                Console.Title = DateTime.Now.ToString("HH:mm:ss ffff");
+                Thread.Sleep(100);
+            }
+        }
+
+        private static void PrintThreadInfo()
+        {
+            var thread = Thread.CurrentThread;
+            Console.WriteLine($"id: {thread.ManagedThreadId};name: {thread.Name};priority: {thread.Priority}");
+        }
+        }
 }
